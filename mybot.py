a372f9fac9fa489754dec3190c234b84b3cb3b8a
PavelPatPy <pavel.pavel.pavel235@gmail.com>
2023-05-20 14:25:55 +0300

    little change in mybot.py

---
+++ mybot.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
