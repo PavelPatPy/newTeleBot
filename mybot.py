3c2c1f133d8649b346d9c3357e5ca83fe2ea66ad
PavelPatPy <pavel.pavel.pavel235@gmail.com>
2023-05-20 21:41:55 +0300

    little change in mybot.py+

--- mybot.py
+++ mybot.py
@@ -4,6 +4,23 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+kb = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+b1 = types.KeyboardButton(text='button1')
+b2 = types.KeyboardButton(text='button2')
+b3 = types.KeyboardButton(text='button3')
+b4 = types.KeyboardButton(text='button4')
+kb.add(b1, b2, b3)
+
+
+@dp.message_handler(commands=['start'])
+async def start_handler(massage: types.Message):
+    await massage.answer('its help bot', reply_markup=kb)
+
+
+@dp.message_handler(commands=['welcome', 'about'])
+async def cmd_handler(massage: types.Message):
+    await massage.answer('its help bot')
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
